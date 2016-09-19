73204abafdd3bdd0afa8d759324a2ce899e2a294
Jean-Michel Gigault <jgigault@student.42.fr>
2016-09-19 15:43:42 +0200

    rename ApiTester with TestHelper + dissociate seedDb and startServer

--- test/api/v1/services.controller.test.js
+++ test/api/v1/services.controller.test.js
@@ -1,26 +1,27 @@
 'use strict';
 
-const apiTester = require('../../test.helper.js'),
-  ApiTester = new apiTester(),
-  request = ApiTester.request;
+const testHelper = require('../../test.helper.js'),
+  TestHelper = new testHelper(),
+  request = TestHelper.request;
 
 describe('Services', function () {
 
-  before(ApiTester.before);
-  after(ApiTester.after);
+  before(TestHelper.startServer);
+  before(TestHelper.seedDb);
+  after(TestHelper.stopServer);
 
   describe('Authenticated service', function () {
 
-    before(ApiTester.authorize);
+    before(TestHelper.authorize);
 
     describe('Collection', function() {
 
       it('show list of public services', function (done) {
         request()
           .get('/api/v1/services')
-          .query({access_token: ApiTester.getAccessToken()})
+          .query({access_token: TestHelper.getAccessToken()})
           .end(function (err, res) {
-            ApiTester.checkResponse(res);
+            TestHelper.checkResponse(res);
             res.body.data.should.have.lengthOf(1);
             res.body.data[0].id.should.eql('my_client_id');
             res.body.data[0].type.should.eql('services');
@@ -37,8 +38,8 @@ describe('Services', function () {
 
       it('show my own service even if private', function (done) {
         // make my own service to be private and check if it is really mine
-        const own_service = ApiTester.getServiceByClientId('my_client_id');
-        own_service.clientId.should.eql(ApiTester.getClientId());
+        const own_service = TestHelper.getServiceByClientId('my_client_id');
+        own_service.clientId.should.eql(TestHelper.getClientId());
         own_service.public = false;
         own_service.save(function(err) {
           if (err)
@@ -47,9 +48,9 @@ describe('Services', function () {
           // then try to request through API
           request()
             .get('/api/v1/services')
-            .query({access_token: ApiTester.getAccessToken()})
+            .query({access_token: TestHelper.getAccessToken()})
             .end(function (err, res) {
-              ApiTester.checkResponse(res);
+              TestHelper.checkResponse(res);
               res.body.data.should.have.lengthOf(1);
               res.body.data[0].id.should.eql('my_client_id');
               res.body.data[0].type.should.eql('services');
@@ -76,9 +77,9 @@ describe('Services', function () {
       it('show public service by id', function (done) {
         request()
           .get('/api/v1/services/my_client_id')
-          .query({access_token: ApiTester.getAccessToken()})
+          .query({access_token: TestHelper.getAccessToken()})
           .end(function (err, res) {
-            ApiTester.checkResponse(res, {isCollection: false});
+            TestHelper.checkResponse(res, {isCollection: false});
             res.body.data.id.should.eql('my_client_id');
             res.body.data.type.should.eql('services');
             res.body.data.attributes.should.eql({
@@ -94,15 +95,15 @@ describe('Services', function () {
 
       it('do not show private service by id', function (done) {
         // firstly check if 'my_client_id_private' is really a private service
-        const private_service = ApiTester.getServiceByClientId('my_client_id_private');
+        const private_service = TestHelper.getServiceByClientId('my_client_id_private');
         private_service.public.should.eql(false);
 
         // then try to request through API
         request()
           .get('/api/v1/services/my_client_id_private')
-          .query({access_token: ApiTester.getAccessToken()})
+          .query({access_token: TestHelper.getAccessToken()})
           .end(function (err, res) {
-            ApiTester.checkResponse(res, {isSuccess: false, status: 404});
+            TestHelper.checkResponse(res, {isSuccess: false, status: 404});
             res.body.errors.should.include('not_found');
             done();
           });
@@ -110,8 +111,8 @@ describe('Services', function () {
 
       it('show my own service even if private', function (done) {
         // make my own service to be private and check if it is really mine
-        const own_service = ApiTester.getServiceByClientId('my_client_id');
-        own_service.clientId.should.eql(ApiTester.getClientId());
+        const own_service = TestHelper.getServiceByClientId('my_client_id');
+        own_service.clientId.should.eql(TestHelper.getClientId());
         own_service.public = false;
         own_service.save(function(err) {
           if (err)
@@ -120,9 +121,9 @@ describe('Services', function () {
           // then try to request through API
           request()
             .get('/api/v1/services/my_client_id')
-            .query({access_token: ApiTester.getAccessToken()})
+            .query({access_token: TestHelper.getAccessToken()})
             .end(function (err, res) {
-              ApiTester.checkResponse(res, {isCollection: false});
+              TestHelper.checkResponse(res, {isCollection: false});
               res.body.data.id.should.eql('my_client_id');
               res.body.data.type.should.eql('services');
               res.body.data.attributes.should.eql({
