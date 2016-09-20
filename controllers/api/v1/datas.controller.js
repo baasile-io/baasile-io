5200a6c1e3446a14854d5e1ab523160021eadf31
bosulliv42 <bosulliv@student.42.fr>
2016-09-20 12:48:22 +0200

    step search data and or value comas ok

--- controllers/api/v1/datas.controller.js
+++ controllers/api/v1/datas.controller.js
@@ -96,80 +96,84 @@ function ServicesController(options) {
 
   function getDefaultOr(key, array, prefix)
   {
-	  var jsonRes = [];
-	  jsonRes[$or] = [];
-	  array.forEach(function(value)
-	  {
-		  var jsonval = {};
-		  if (value[0] == '$')
-			jsonval[key] = getConditionConstruct(key, value, prefix);
-		  else
-			jsonval[prefix+"."+key] = value;
-		  jsonRes[$or].push(jsonval);
-	  });
-	  return jsonRes;
+  	var jsonRes = new Array();
+  	array.forEach(function(value)
+  	{
+  		var jsonval = {};
+  		if (value[0] == '$')
+  		  jsonval[key] = getConditionConstruct(value, prefix);
+  		else
+  		  jsonval[prefix+"."+key] = value;
+		jsonRes.push(jsonval);
+  	});
+  	return jsonRes;
   }
 
   function getDefaultAnd(key, array, prefix)
   {
-	  var jsonRes = [];
-	  jsonRes[$and] = [];
-	  var jsonval = {};
-	  if (array[0][0] == '$') {
-		jsonval[key] = getConditionConstruct(key, array[0], prefix);
-	  }
-	  else {
-		jsonval[prefix+"."+key] = array[0];
-	  }
-	  jsonRes[$and].push(jsonval);
-	  return jsonRes;
+
+  	var jsonRes = new Array();
+  	var jsonval = {};
+  	if (array[0][0] == '$') {
+  	  jsonval[key] = getConditionConstruct(array[0], prefix);
+  	}
+  	else {
+  	  jsonval[prefix+"."+key] = array[0];
+  	}
+	jsonRes.push(jsonval);
+  	return jsonRes;
   }
+
 
   function getComasValueOrValue(key, val, prefix)
   {
-	  var jsonRes = {};
-	  var array = val.split(',');
-	  if (array.length > 1)
-	  {
-		jsonRes getDefaultOr(key, array, prefix);
-	 }
-	 else if (array.length == 1)
-	 {
-		return getDefaultAnd(key, array, prefix);
-	 }
-	 return undefined;
+	 var jsontab = [];
+  	var jsonRes = {};
+  	var array = val.split(',');
+  	if (array.length > 1)
+  	{
+  	  jsonRes['$or'] = getDefaultOr(key, array, prefix);
+  	}
+  	else if (array.length == 1)
+  	{
+  	  jsonRes['$or'] = getDefaultAnd(key, array, prefix);
+  	}
+	jsontab.push(jsonRes);
+  	return jsontab;
   }
 
-  function getConditionConstruct(key, value, prefix)
+  function getConditionConstruct(array, prefix)
   {
-	  var jsonObj = {};
-	  jsonObj = getComasValueOrValue(key, val, prefix);
+  	var jsontab = [];
+  	Object.keys(array).forEach(function(key)
+  	{
+	  var jsonRes = {};
+  	  if (key[0] == '$') {
+  		 jsonRes[key] = getConditionConstruct(array[key], prefix);
+  	  }
+  	  else {
+  		jsonRes["$and"] = getComasValueOrValue(key, array[key], prefix);
+  	  }
+	  jsontab.push(jsonRes);
+  	});
+  	return jsontab;
   }
 
   function getConstructedJson(req, res, prefix)
   {
     var jsonRes = {};
     jsonRes["route"] = req.data.route._id;
-	if (typeof res._request.params.filter !== 'undefined')
-	{
-	 	logger.info("param filter ok");
-	    var tab = [];
-	    Object.keys(res._request.params.filter).forEach(function(key)
-		{
-		  if (key[0] == '$') {
-			 jsonRes[key] = tab.push.getConditionConstruct(key, res._request.params.filter[key], prefix);
-		  }
-		  else {
-		  	jsonRes = getComasValue(key, res._request.params.filter[key], prefix);
-		  }
-	    });
-	}
-	return jsonRes;
+    if (typeof res._request.params.filter !== 'undefined')
+    {
+  	  jsonRes["$and"] = getConditionConstruct(res._request.params.filter, prefix);
+    }
+    return jsonRes;
   }
 
   function requestGet(req, res, next) {
     var dataResult = [];
-	var jsonSearch = getConstructedJson(req, res);
+	var jsonSearch = getConstructedJson(req, res, "data");
+
 	logger.info("test");
 	logger.info(JSON.stringify(jsonSearch));
 	DataModel
