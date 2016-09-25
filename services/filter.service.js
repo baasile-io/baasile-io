eac1b4cb2813d49d36a1c2dad68545b617b471da
bosulliv42 <bosulliv@student.42.fr>
2016-09-25 19:28:24 +0200

    test en mode array en get ok

--- services/filter.service.js
+++ services/filter.service.js
@@ -8,18 +8,18 @@ function FilterService(options)
 {
   const CONDITIONAL_OPERATORS = ['$and', '$or'];
 
-  const COND_TYPES = [
-    {'ALL': []},
-    {'ID': []},
-    {'STRING': []},
-    {'NUMERIC': ["$gt", "$lt", "$gte", "$lte"]},
-    {'PERCENT': ["$gt", "$lt", "$gte", "$lte"]},
-    {'AMOUNT': ["$gt", "$lt", "$gte", "$lte"]},
-    {'BOOLEAN': []},
-    {'DATE': ["$gt", "$lt", "$gte", "$lte"]},
-    {'ENCODED': []},
-    {'JSON': []}
-    ];
+  const COND_TYPES = {
+    'ALL': [],
+    'ID': [],
+    'STRING': [],
+    'NUMERIC': ["$gt", "$lt", "$gte", "$lte"],
+    'PERCENT': ["$gt", "$lt", "$gte", "$lte"],
+    'AMOUNT': ["$gt", "$lt", "$gte", "$lte"],
+    'BOOLEAN': [],
+    'DATE': ["$gt", "$lt", "$gte", "$lte"],
+    'ENCODED': [],
+    'JSON': []
+	};
   options = options || {};
   const logger = options.logger;
 
@@ -59,16 +59,20 @@ function FilterService(options)
 
   function getValIfValExist( value, listfields)
   {
-    return value;
-    if (searchByVal("name", value, listfields) !== undefined)
-      return value;
+	  logger.info("try\"" + value + "\" in base model");
+    var typeTab = searchByVal("name", value, listfields);
+	 logger.info("type tabe\"" + JSON.stringify(typeTab) + "\"");
+    if ( typeTab !== undefined)
+	{
+		return value;
+  	}
     else {
       logger.info("field \"" + value + "\" is not in base model");
       return (undefined);
     }
   }
 
-  function getTypeOf(value, listfields)
+  function getTableTypeOf(value, listfields)
   {
     if ((value === undefined) || Array.isArray(value) || (value[0] == '$'))
       return CONDITIONAL_OPERATORS;
@@ -77,18 +81,42 @@ function FilterService(options)
       var objson = searchByVal("name", getValIfValExist( value, listfields), listfields);
 	  if (objson === undefined)
 	  	return undefined;
-      return objson.key;
+	  return COND_TYPES[objson.key];
+    }
+  }
+
+  function getTypeOf(value, listfields)
+  {
+    if ((value === undefined) || Array.isArray(value) || (value[0] == '$'))
+      return undefined;
+    else
+    {
+      var objson = searchByVal("name", getValIfValExist( value, listfields), listfields);
+	  if (objson === undefined)
+	  	return undefined;
+	  return objson.key;
     }
   }
 
   function tryKeyByType(key, value, listfields)
   {
-	var type = getTypeOf(value, listfields);
+	var type = getTableTypeOf(value, listfields);
+	logger.info("type : => "+JSON.stringify(type));
     if (type !== undefined && (type.indexOf(key) != -1)) {
       return true;
     }
     logger.info("condition \"" + key + "\" of value \"" + value + "\" of type \"" + type + "\" is not allowed in this case");
     return false;
+  }
+
+  function getValueByFieldType(val, keyname, listfields)
+  {
+	  var type = getTypeOf(keyname, listfields);
+	  if (type == 'NUMERIC' || type == 'PERCENT' || type == 'AMOUNT')
+	  	return Number(val);
+	 else {
+	 	return val;
+	 }
   }
 
   function getConditionAfter(val, obj, listfields)
@@ -97,7 +125,7 @@ function FilterService(options)
   	Object.keys(obj).forEach(function(key)
   	{
 	  if (key[0] === '$' && tryKeyByType(key, val, listfields)) {
-		  jsonRes[key] = obj[key];
+		  jsonRes[key] = getValueByFieldType(obj[key], val, listfields);
 	  }
 	  else {
 	  	return undefined;
@@ -122,7 +150,11 @@ function FilterService(options)
 		 var kval = getValIfValExist(key, listfields);
 		 if (kval === undefined)
 		 	return (undefined);
-		 jsonRes[kval] = (typeof array[kval] === 'object') ? getConditionAfter(kval, array[kval], listfields) : array[kval] ;
+		if (Array.isArray(array[kval]))
+		logger.info("it s an array");
+		if (typeof array[kval] === 'object')
+		logger.info("it s an object");
+		 jsonRes[kval] = (typeof array[kval] === 'object' ||  Array.isArray(array[kval])) ? getConditionAfter(kval, array[kval], listfields) : getValueByFieldType(array[kval], kval, listfields); ;
       }
 	  else
 	  {
