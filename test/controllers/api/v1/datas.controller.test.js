caf1b76e72d3ce167f2499608a8d3ef6fbb07a49
bosulliv42 <bosulliv@student.42.fr>
2016-10-03 16:33:55 +0200

    filter v1           + test

--- test/controllers/api/v1/datas.controller.test.js
+++ test/controllers/api/v1/datas.controller.test.js
@@ -14,39 +14,6 @@ describe('Datas', function () {
 
     before(TestHelper.authorize);
   
-    // const filter1 = "filter[data.field1]=third";
-    // const filter2 = "filter[data.field3][$gt]=50";
-    // const filter3 = "filter[data.field3][$lt]=50";
-    // const filter4 = "filter[data.field3][$gte]=30&filter[data.field3][$lte]=80";
-    // const filter5 = "filter[$or][$and][0][$or][field1]=third&filter[$or][$and][0][$or][field1]=fours&filter[$or][$and][1][$or][field2]=test3&filter[$or][$and][1][$or][field2]=test2&filter[$or][$and][field3][$gte]=43&filter[$or][$and][field3][$lte]=80";
-  
-  
-    const filter = [
-      "",
-      "filter[data.field1]=third",
-      "filter[data.field3][$gt]=50",
-      "filter[data.field3][$lt]=50",
-      "filter[data.field3][$gte]=30&filter[data.field3][$lte]=80",
-      "filter[$or][$and][0][$or][field1]=third&filter[$or][$and][0][$or][field2]=test3&filter[$or][$and][1][$or][field2]=test2&filter[$or][$and][field3][$gte]=43&filter[$or][$and][field3][$lte]=80"
-    ];
-    
-    
-    it('test transformation', function (done) {
-      for (var i = 0; i < filter.length; ++i) {
-        console.log(" ---- filter "+i+ " ----");
-        console.log(JSON.stringify(filter[i]));
-        console.log(" -----------------------");
-        request()
-          .get('/api/v1/services/my_client_id/relationships/collections/my_route_id1/relationships/donnees?'+filter[i])
-          .query({access_token: TestHelper.getAccessToken()})
-          .end(function (err, res) {
-            TestHelper.checkResponse(res);
-            done();
-          });
-      }
-    });
-  
-  
     it('show list of public data', function (done) {
       request()
         .get('/api/v1/services/my_client_id/relationships/collections/my_route_id1/relationships/donnees')
@@ -65,13 +32,74 @@ describe('Datas', function () {
           });
           done();
         });
-    });
+     });
   
     describe('Filter service', function () {
-    
-      it('simple filter one field value', function (done) {
+  
+      const datadb = {
+        my_data_id1: {field1: 'first string', field2: 'second string', field3: 12, field4: {'key': 'value'}, field5: 75},
+        my_data_id2: {field1: 'second', field2: 'test1', field3: 1, field4: {'key': 'value'}, field5: 2},
+        my_data_id3: {field1: 'third', field2: 'test2', field3: 70, field4: {'key': 'value'}, field5: 85},
+        my_data_id4: {field1: 'fours', field2: 'test3', field3: 43, field4: {'key': 'value'}, field5: 35},
+        my_data_id5: {field1: 'fives', field2: 'test4', field3: 80, field4: {'key': 'value'}, field5: 2},
+        my_data_id6: {field1: 'sixs', field2: 'test5', field3: 125, field4: {'key': 'value'}, field5: 185}
+      };
+  
+      const filterClassic = [
+        { testname: "test AND", res:["my_data_id1"], filter : "filter[$and][data.field1]=first string&filter[$and][data.field3]=12&filter[$and][data.field5][$gt]=72&filter[$and][data.field5][$lt]=90"}, //my_data_id1
+        { testname: "test OR", res:["my_data_id1", "my_data_id2", "my_data_id3", "my_data_id5", "my_data_id6"], filter : "filter[$or][data.field1]=second&filter[$or][data.field3]=12&filter[$or][data.field3]=12&filter[$or][data.field5]=85&filter[$or][data.field1]=sixs&filter[$or][data.field2]=test4"}, //my_data_id1 //my_data_id2 //my_data_id3 //my_data_id5 //my_data_id6
+        { testname: "test inseption AND OR", res:["my_data_id2"], filter : "filter[$and][$or][data.field1]=second&filter[$and][$or][data.field1]=sixs&filter[$and][$or][data.field1]=fives&filter[$and][data.field3][$lt]=20"}, //my_data_id2
+        { testname: "test multiple and inseption AND OR", res:["my_data_id2"], filter : "filter[$and][0][$or][data.field1]=second&filter[$and][0][$or][data.field1]=third&filter[$and][0][$or][data.field1]=fours&filter[$and][0][$or][data.field1]=fives&filter[$and][0][$or][data.field1]=sixs&filter[$and][1][$or][data.field3]=12&filter[$and][1][$or][data.field3]=1&filter[$and][1][$or][data.field3]=70&filter[$and][1][$or][data.field3]=125&filter[$and][2][$or][data.field5]=35&filter[$and][2][$or][data.field5]=2&filter[$and][2][$or][data.field5]=75"}, //my_data_id2
+        { testname: "test $gt $lt $gte $lte", res:["my_data_id3", "my_data_id4"], filter : "filter[data.field3][$gt]=42&filter[data.field3][$lt]=71&filter[data.field5][$gte]=35&filter[data.field5][$lte]=85"}, //my_data_id3 //my_data_id4
+        { testname: "test $eq -> $in", res:["my_data_id1", "my_data_id2", "my_data_id3" ], filter : "filter[data.field3][$eq]=12&filter[data.field3][$eq]=1&filter[data.field3][$eq]=70"},//my_data_id1 //my_data_id2 //my_data_id3
+        { testname: "test $ne -> $nin", res:["my_data_id4", "my_data_id5", "my_data_id6"], filter : "filter[data.field3][$ne]=12&filter[data.field3][$ne]=1&filter[data.field3][$ne]=70"},//my_data_id4 //my_data_id5 //my_data_id6
+        { testname: "test $in", res:["my_data_id1", "my_data_id2", "my_data_id3"], filter : "filter[data.field3][$in]=12&filter[data.field3][$in]=1&filter[data.field3][$in]=70"},//my_data_id1 //my_data_id2 //my_data_id3
+        { testname: "test $nin", res:["my_data_id4", "my_data_id5", "my_data_id6"], filter : "filter[data.field3][$nin]=12&filter[data.field3][$nin]=1&filter[data.field3][$nin]=70"} //my_data_id4 //my_data_id5 //my_data_id6
+      ]
+  
+      const filter = [
+        "",
+        "filter[data.field1]=third",
+        "filter[data.field3][$gt]=50",
+        "filter[data.field3][$lt]=50",
+        "filter[data.field3][$gte]=30&filter[data.field3][$lte]=80",
+        "filter[$or][$and][0][$or][data.field1]=third&filter[$or][$and][0][$or][data.field2]=test3&filter[$or][$and][1][$or][data.field2]=test2&filter[$or][$and][2][data.field3][$gte]=56&filter[$or][$and][2][data.field3][$lte]=71",
+        "filter[$or][data.field1]=third&filter[$or][data.field2]=test3&filter[$or][data.field3]=90",
+        "filter[$or][data.field1]=third&filter[$or][data.field2]=test3&filter[$or][data.field2]=test2",
+        "filter[$and][data.field1]=third&filter[$and][data.field2]=test2&filter[$and][data.field3]=70",
+        "filter[$and][$or][data.field1]=third&filter[$and][$or][data.field2]=test3&filter[$and][data.field3]=90",
+        "filter[$or][$and][0][$or][data.field1]=third&filter[$or][$and][0][$or][data.field2]=test3&filter[$or][$and][1][$or][data.field2]=test2&filter[$or][$and][2][data.field3][$gte]=23&filter[$or][$and][2][data.field3][$lte]=69"
+      ]
+  
+  
+  
+      filterClassic.forEach(function(objfilterclassic) {
+        it(objfilterclassic.testname, function (done) {
+          request()
+            .get('/api/v1/services/my_client_id/relationships/collections/my_route_id1/relationships/donnees?' + objfilterclassic.filter)
+            .query({access_token: TestHelper.getAccessToken()})
+            .end(function (err, res) {
+              TestHelper.checkResponse(res);
+              res.body.data.should.have.lengthOf(objfilterclassic.res.length);
+              for (var j = 0; j < objfilterclassic.res.length; ++j) {
+                res.body.data[j].id.should.eql(objfilterclassic.res[j]);
+                res.body.data[j].type.should.eql('donnees');
+                res.body.data[j].attributes.should.eql({
+                  field1: datadb[objfilterclassic.res[j]].field1,
+                  field2: datadb[objfilterclassic.res[j]].field2,
+                  field3: datadb[objfilterclassic.res[j]].field3,
+                  field4: datadb[objfilterclassic.res[j]].field4,
+                  field5: datadb[objfilterclassic.res[j]].field5
+                });
+              }
+              done();
+            });
+        });
+      });
+      
+      it('simple filter 1', function (done) {
         request()
-          .get('/api/v1/services/my_client_id/relationships/collections/my_route_id1/relationships/donnees?'+filter[0])
+          .get('/api/v1/services/my_client_id/relationships/collections/my_route_id1/relationships/donnees?'+filter[1])
           .query({access_token: TestHelper.getAccessToken()})
           .end(function (err, res) {
             TestHelper.checkResponse(res);
@@ -88,10 +116,10 @@ describe('Datas', function () {
             done();
           });
       });
-  
-      it('simple filter one field $gt NUMERIC', function (done) {
+
+      it('simple filter 2', function (done) {
         request()
-          .get('/api/v1/services/my_client_id/relationships/collections/my_route_id1/relationships/donnees?'+filter[1])
+          .get('/api/v1/services/my_client_id/relationships/collections/my_route_id1/relationships/donnees?'+filter[2])
           .query({access_token: TestHelper.getAccessToken()})
           .end(function (err, res) {
             TestHelper.checkResponse(res);
@@ -126,10 +154,10 @@ describe('Datas', function () {
             done();
           });
       });
-  
-      it('simple filter one field $lt NUMERIC', function (done) {
+
+      it('simple filter 3', function (done) {
         request()
-          .get('/api/v1/services/my_client_id/relationships/collections/my_route_id1/relationships/donnees?'+filter[2])
+          .get('/api/v1/services/my_client_id/relationships/collections/my_route_id1/relationships/donnees?'+filter[3])
           .query({access_token: TestHelper.getAccessToken()})
           .end(function (err, res) {
             TestHelper.checkResponse(res);
@@ -164,10 +192,10 @@ describe('Datas', function () {
             done();
           });
       });
-  
-      it('simple filter $gte $lte NUMERIC', function (done) {
+
+      it('simple filter 4', function (done) {
         request()
-          .get('/api/v1/services/my_client_id/relationships/collections/my_route_id1/relationships/donnees?'+filter[3])
+          .get('/api/v1/services/my_client_id/relationships/collections/my_route_id1/relationships/donnees?'+filter[4])
           .query({access_token: TestHelper.getAccessToken()})
           .end(function (err, res) {
             TestHelper.checkResponse(res);
@@ -202,112 +230,107 @@ describe('Datas', function () {
             done();
           });
       });
-  
-      // it('simple filter $in $nin', function (done) {
-      //   request()
-      //     .get('/api/v1/services/my_client_id/relationships/collections/my_route_id1/relationships/donnees?filter[data.field3][$and][$or][$ne]=70&filter[data.field3][$and][$or][$ne]=43&filter[data.field3][$and][$or][$ne]=80')
-      //     .query({access_token: TestHelper.getAccessToken()})
-      //     .end(function (err, res) {
-      //       TestHelper.checkResponse(res);
-      //       res.body.data.should.have.lengthOf(3);
-      //       res.body.data[0].id.should.eql('my_data_id3');
-      //       res.body.data[0].type.should.eql('donnees');
-      //       res.body.data[0].attributes.should.eql({
-      //         field1: 'third',
-      //         field2: 'test2',
-      //         field3: 70,
-      //         field4: {'key' : 'value'},
-      //         field5: 85
-      //       });
-      //       res.body.data[1].id.should.eql('my_data_id4');
-      //       res.body.data[1].type.should.eql('donnees');
-      //       res.body.data[1].attributes.should.eql({
-      //         field1: 'fours',
-      //         field2: 'test3',
-      //         field3: 43,
-      //         field4: {'key' : 'value'},
-      //         field5: 35
-      //       });
-      //       res.body.data[2].id.should.eql('my_data_id5');
-      //       res.body.data[2].type.should.eql('donnees');
-      //       res.body.data[2].attributes.should.eql({
-      //         field1: 'fives',
-      //         field2: 'test4',
-      //         field3: 80,
-      //         field4: {'key' : 'value'},
-      //         field5: 2
-      //       });
-      //       done();
-      //     });
-      // });
-      
-      it('advanced filter $gte $lte NUMERIC', function (done) {
+
+      it('simple filter 5', function (done) {
         request()
-          .get('/api/v1/services/my_client_id/relationships/collections/my_route_id1/relationships/donnees?'+filter[4])
+          .get('/api/v1/services/my_client_id/relationships/collections/my_route_id1/relationships/donnees?'+filter[5])
           .query({access_token: TestHelper.getAccessToken()})
           .end(function (err, res) {
             TestHelper.checkResponse(res);
-            res.body.data.should.have.lengthOf(6);
-            res.body.data[0].id.should.eql('my_data_id1');
+            res.body.data.should.have.lengthOf(1);
+            res.body.data[0].id.should.eql('my_data_id3');
             res.body.data[0].type.should.eql('donnees');
             res.body.data[0].attributes.should.eql({
-              field1: 'first string',
-              field2: 'second string',
-              field3: 12,
-              field4: {'key' : 'value'},
-              field5: 75
-            });
-            res.body.data[1].id.should.eql('my_data_id2');
-            res.body.data[1].type.should.eql('donnees');
-            res.body.data[1].attributes.should.eql({
-              field1: 'second',
-              field2: 'test1',
-              field3: 1,
-              field4: {'key' : 'value'},
-              field5: 2
-            });
-            res.body.data[2].id.should.eql('my_data_id3');
-            res.body.data[2].type.should.eql('donnees');
-            res.body.data[2].attributes.should.eql({
               field1: 'third',
               field2: 'test2',
               field3: 70,
               field4: {'key' : 'value'},
               field5: 85
             });
-            res.body.data[3].id.should.eql('my_data_id4');
-            res.body.data[3].type.should.eql('donnees');
-            res.body.data[3].attributes.should.eql({
+            done();
+          });
+      });
+
+      it('simple filter 6', function (done) {
+        request()
+          .get('/api/v1/services/my_client_id/relationships/collections/my_route_id1/relationships/donnees?'+filter[6])
+          .query({access_token: TestHelper.getAccessToken()})
+          .end(function (err, res) {
+            TestHelper.checkResponse(res);
+            res.body.data.should.have.lengthOf(2);
+            res.body.data[0].id.should.eql('my_data_id3');
+            res.body.data[0].type.should.eql('donnees');
+            res.body.data[0].attributes.should.eql({
+              field1: 'third',
+              field2: 'test2',
+              field3: 70,
+              field4: {'key' : 'value'},
+              field5: 85
+            });
+            res.body.data[1].id.should.eql('my_data_id4');
+            res.body.data[1].type.should.eql('donnees');
+            res.body.data[1].attributes.should.eql({
               field1: 'fours',
               field2: 'test3',
               field3: 43,
               field4: {'key' : 'value'},
               field5: 35
             });
-            res.body.data[4].id.should.eql('my_data_id5');
-            res.body.data[4].type.should.eql('donnees');
-            res.body.data[4].attributes.should.eql({
-              field1: 'fives',
-              field2: 'test4',
-              field3: 80,
+            done();
+          });
+      });
+
+      it('advanced filter 7', function (done) {
+        request()
+          .get('/api/v1/services/my_client_id/relationships/collections/my_route_id1/relationships/donnees?'+filter[7])
+          .query({access_token: TestHelper.getAccessToken()})
+          .end(function (err, res) {
+            TestHelper.checkResponse(res);
+            res.body.data.should.have.lengthOf(2);
+            res.body.data[0].id.should.eql('my_data_id3');
+            res.body.data[0].type.should.eql('donnees');
+            res.body.data[0].attributes.should.eql({
+              field1: 'third',
+              field2: 'test2',
+              field3: 70,
               field4: {'key' : 'value'},
-              field5: 2
+              field5: 85
             });
-            res.body.data[5].id.should.eql('my_data_id6');
-            res.body.data[5].type.should.eql('donnees');
-            res.body.data[5].attributes.should.eql({
-              field1: 'sixs',
-              field2: 'test5',
-              field3: 125,
+            res.body.data[1].id.should.eql('my_data_id4');
+            res.body.data[1].type.should.eql('donnees');
+            res.body.data[1].attributes.should.eql({
+              field1: 'fours',
+              field2: 'test3',
+              field3: 43,
               field4: {'key' : 'value'},
-              field5: 185
+              field5: 35
             });
             done();
           });
       });
-      
+
+      it('advanced filter 8', function (done) {
+        request()
+          .get('/api/v1/services/my_client_id/relationships/collections/my_route_id1/relationships/donnees?'+filter[8])
+          .query({access_token: TestHelper.getAccessToken()})
+          .end(function (err, res) {
+            TestHelper.checkResponse(res);
+            res.body.data.should.have.lengthOf(1);
+            res.body.data[0].id.should.eql('my_data_id3');
+            res.body.data[0].type.should.eql('donnees');
+            res.body.data[0].attributes.should.eql({
+              field1: 'third',
+              field2: 'test2',
+              field3: 70,
+              field4: {'key' : 'value'},
+              field5: 85
+            });
+            done();
+          });
+      });
+
     });
-    
+
   });
   
   
