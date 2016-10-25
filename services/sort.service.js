46e1c1582dc0ad775703a5b0af0f02edb4d44351
bosulliv42 <bosulliv@e2r4p21.42.fr>
2016-10-25 13:35:12 +0200

    error renamed and functions put in scop sort

--- services/sort.service.js
+++ services/sort.service.js
@@ -4,48 +4,48 @@ const CONFIG = require('../config/app.js');
 
 module.exports = SortService;
 
-function getTabByKeyVal(key, val, myArray, myarray2) {
-  if (key === undefined || val === undefined || (myArray === undefined && myarray2 === undefined) ) {
-    return undefined;
-  }
-  if (myArray !== undefined) {
-    for (var i = 0; i < myArray.length; i++) {
-      if (myArray[i][key] === val) {
-        return myArray[i];
+function SortService(options) {
+  function getTabByKeyVal(key, val, myArray, myarray2) {
+    if (key === undefined || val === undefined || (myArray === undefined && myarray2 === undefined) ) {
+      return undefined;
+    }
+    if (myArray !== undefined) {
+      for (var i = 0; i < myArray.length; i++) {
+        if (myArray[i][key] === val) {
+          return myArray[i];
+        }
       }
     }
-  }
-  if (myarray2 !== undefined) {
-    for (var i = 0; i < myarray2.length; i++) {
-      if (myarray2[i][key] === val) {
-        return myarray2[i];
-      }
-      else if (("aliases" in myarray2[i]))
-      {
-        for (var j = 0; j < myarray2[i]["aliases"].length; j++) {
-          if (myarray2[i]["aliases"][j] === val) {
-            return myarray2[i];
+    if (myarray2 !== undefined) {
+      for (var i = 0; i < myarray2.length; i++) {
+        if (myarray2[i][key] === val) {
+          return myarray2[i];
+        }
+        else if (("aliases" in myarray2[i]))
+        {
+          for (var j = 0; j < myarray2[i]["aliases"].length; j++) {
+            if (myarray2[i]["aliases"][j] === val) {
+              return myarray2[i];
+            }
           }
         }
       }
     }
+    return undefined;
   }
-  return undefined;
-}
-
-function getValIfValExistInArray(value, param) {
-  if (param["model"] === undefined && param["modelName"] === undefined)
-    return value;
-  var typeTab = getTabByKeyVal("name", value, param["model"], param["modelName"]);
-  if (typeTab !== undefined) {
-    return value;
+  
+  function getValIfValExistInArray(value, param) {
+    if (param["model"] === undefined && param["modelName"] === undefined)
+      return value;
+    var typeTab = getTabByKeyVal("name", value, param["model"], param["modelName"]);
+    if (typeTab !== undefined) {
+      return value;
+    }
+    else {
+      return (undefined);
+    }
   }
-  else {
-    return (undefined);
-  }
-}
-
-function SortService(options) {
+  
   function checkSortParam(sorts, param)
   {
     var objRes = {};
@@ -62,19 +62,19 @@ function SortService(options) {
           tab[key] = tab[key].substring(1);
         }
         if (tab[key] in objRes) {
-          param["errors"].push("you allready set a sort by the field" + tab[key] + " you can only set one type of sort for one field");
+          param["errors"].push("sort by field \"" + tab[key] + "\" has already been set");
         }
         else if (getValIfValExistInArray(tab[key], param)) {
           objRes[tab[key]] = sign;
         }
         else {
-          param["errors"].push("the name " + tab[key] + " is not in the field model");
+          param["errors"].push("the field \"" + tab[key] + "\" is not in the model");
         }
         return true;
       });
     }
     else {
-      param["errors"].push("sort need to be a string : ?sort=value1 or ?sort=-value1 or ?sort=value1,value2,value3");
+      param["errors"].push("the parameter \"sort\" must be a string: ?sort=value1 or ?sort=-value1 or ?sort=value1,value2,value3");
     }
     return objRes;
   }
