478bc31764a4d47bebd9449f1f2f64352450ce11
bosulliv42 <bosulliv@student.42.fr>
2017-01-05 11:40:58 +0100

    modes query param to enum

--- spec/helpers/query_parameter_helper_spec.rb
+++ spec/helpers/query_parameter_helper_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 # Specs in this file have access to a helper object that includes
-# the QueryParameterHelper. For example:
+# the QueryParametersHelper. For example:
 #
-# describe QueryParameterHelper do
+# describe QueryParametersHelper do
 #   describe "string concat" do
 #     it "concats two strings with spaces" do
 #       expect(helper.concat_strings("this","that")).to eq("this that")
 #     end
 #   end
 # end
-RSpec.describe QueryParameterHelper, type: :helper do
+RSpec.describe QueryParametersHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
