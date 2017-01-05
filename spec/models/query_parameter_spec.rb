245ca96a065bd3927b3c57ddce021d08ccb583f7
bosulliv42 <bosulliv@student.42.fr>
2017-01-05 13:54:49 +0100

    petit test model queryParameter

--- spec/models/query_parameter_spec.rb
+++ spec/models/query_parameter_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe QueryParameter, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @query_parameter = create :query_parameter
+  end
+
+  describe "attributes" do
+    it "requires a name" do
+      @query_parameter.name = nil
+      expect(@query_parameter.valid?).to be_falsey
+      expect(@query_parameter.errors.messages[:name]).to_not be_empty
+    end
+  end
 end
