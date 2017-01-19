618358b1c9e90a488d44dc410b62779a2db772ae
bosulliv42 <bosulliv@student.42.fr>
2017-01-19 18:46:53 +0100

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  before_action :is_super_admin, only: [:index]
+  before_action :load_user, only: [:show]
+
+  def index
+    @collection = User.all
+  end
+
+  def show
+  end
+
+  def load_user
+    @user = User.find_by_id(params[:id])
+    if current_user.has_role?(:superadmin)
+      if @user.nil?
+        return redirect_to users_path
+      end
+    else
+      if @user.nil? || @user != current_user
+        return redirect_back
+      end
+    end
+  end
+
+  def is_super_admin
+    return head(:forbidden) unless current_user.has_role?(:superadmin)
+  end
+
+end
