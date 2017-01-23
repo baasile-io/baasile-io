5426ee12c071d19e49dfc3d1b8aacc3eb1ca15fb
Jean-Michel Gigault <jgigault@student.42.fr>
2017-01-23 11:22:37 +0100

    fix send_validartion

--- app/mailers/service_notifier.rb
+++ app/mailers/service_notifier.rb
@@ -2,7 +2,8 @@ class ServiceNotifier < ApplicationMailer
 
   def send_validation(service)
     @service = service
-    users = User.find_as(:developer, @service)
+    #users = User.find_as(:developer, @service)
+    users = [@service.user]
     users.each do |u|
       mail( :to => u.email,
             :subject => I18n.t('mailer.service_notifier.send_validation.subject') )
