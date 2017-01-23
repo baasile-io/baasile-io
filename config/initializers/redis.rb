9dce6b34c5bc772197fe107b991b1eaf0765f3d7
Jean-Michel Gigault <jgigault@student.42.fr>
2017-01-23 14:54:57 +0100

    try to fix redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,1 @@
-$redis = Redis::Namespace.new("app", redis: Redis.new({url: ENV['REDIS_PROVIDER']}))
+$redis = Redis::Namespace.new("app", redis: Redis.new(url: ENV.fetch('REDIS_PROVIDER', 'REDIS_URL')))
