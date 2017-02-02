f13e555efaf15b12a505542607639cfe74ce1eb4
Jean-Michel Gigault <jgigault@student.42.fr>
2017-02-02 09:26:13 +0100

    fix uniqueness name proxy

--- app/models/proxy.rb
+++ app/models/proxy.rb
@@ -12,7 +12,7 @@ class Proxy < ApplicationRecord
   accepts_nested_attributes_for :proxy_parameter
 
   validates :name, presence: true, length: {minimum: 2, maximum: 255}
-  validates :name, uniqueness: true, scope: [:service_id]
+  validates :name, uniqueness: {scope: :service_id}
   validates :description, presence: true
 
   scope :authorized, ->(user) { user.has_role?(:superadmin) ? all : find_as(:developer, user) }
