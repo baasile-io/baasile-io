b7c2adba3b260c6ab7d10735eb9340bc90c03504
Jean-Michel Gigault <jgigault@student.42.fr>
2017-02-04 16:43:13 +0100

    tmp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  helper_method :current_company
   helper_method :current_service
   helper_method :current_proxy
   helper_method :current_route
