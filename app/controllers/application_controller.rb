03848bae5ed4433b61eb45270c1cb595e2ed90b3
bosulliv42 <bosulliv@student.42.fr>
2017-02-08 10:21:30 +0100

    check current user if is_active

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # reset captcha code after each request for security
   after_action :reset_last_captcha_code!
+  after_action :check_current_user_is_active
 
   protect_from_forgery with: :exception
 
@@ -9,6 +10,15 @@ class ApplicationController < ActionController::Base
   helper_method :current_proxy
   helper_method :current_route
   helper_method :current_module
+
+  def check_current_user_is_active
+    if !current_user.nil?
+      unless current_user.is_active?
+        sign_out current_user
+        return head(:forbidden)
+      end
+    end
+  end
 
   def authorize_admin!
     unless current_user.is_admin_of?(current_company) || current_user.is_superadmin?
