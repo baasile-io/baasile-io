1a4a73ae980e535ce15975cf1a3fad550a1e46d9
bosulliv42 <bosulliv@student.42.fr>
2017-02-09 09:38:50 +0100

    default user activation false

--- db/migrate/20170207200818_add_activation_user.rb
+++ db/migrate/20170207200818_add_activation_user.rb
@@ -1,5 +1,5 @@
 class AddActivationUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :is_active, :boolean, default: true
+    add_column :users, :is_active, :boolean, default: false
   end
 end
