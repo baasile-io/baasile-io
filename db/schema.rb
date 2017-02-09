925760fbc1b08528ccb213135d575971078040b4
bosulliv42 <bosulliv@student.42.fr>
2017-02-09 09:41:59 +0100

    rollback migrate

--- db/schema.rb
+++ db/schema.rb
@@ -145,12 +145,12 @@ ActiveRecord::Schema.define(version: 20170207200818) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                             default: "",   null: false
-    t.string   "encrypted_password",                default: "",   null: false
+    t.string   "email",                             default: "",    null: false
+    t.string   "encrypted_password",                default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                     default: 0,    null: false
+    t.integer  "sign_in_count",                     default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
@@ -159,11 +159,11 @@ ActiveRecord::Schema.define(version: 20170207200818) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",                   default: 0,    null: false
+    t.integer  "failed_attempts",                   default: 0,     null: false
     t.string   "unlock_token"
     t.datetime "locked_at"
-    t.datetime "created_at",                                       null: false
-    t.datetime "updated_at",                                       null: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
     t.string   "current_subdomain"
     t.string   "first_name"
     t.string   "last_name"
@@ -173,7 +173,7 @@ ActiveRecord::Schema.define(version: 20170207200818) do
     t.string   "unique_session_id",      limit: 20
     t.datetime "last_activity_at"
     t.datetime "expired_at"
-    t.boolean  "is_active",                         default: true
+    t.boolean  "is_active",                         default: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["expired_at"], name: "index_users_on_expired_at", using: :btree
     t.index ["last_activity_at"], name: "index_users_on_last_activity_at", using: :btree
