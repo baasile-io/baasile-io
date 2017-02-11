32effb8841fe0757bf7bd9d37e3eb81f73c24481
Jean-Michel Gigault <jgigault@student.42.fr>
2017-02-11 12:08:36 +0100

    update migration

--- db/migrate/20170207200818_add_activation_user.rb
+++ db/migrate/20170207200818_add_activation_user.rb
@@ -1,5 +1,6 @@
 class AddActivationUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :is_active, :boolean, default: true
+    add_column :users, :is_active, :boolean, default: false
+    User.update_all(is_active: true)
   end
 end
--- db/migrate/20170209144626_add_activation_user_default_false.rb
+++
@@ -1,5 +0,0 @@
-class AddActivationUserDefaultFalse < ActiveRecord::Migration[5.0]
-  def change
-    change_column :users, :is_active, :boolean, default: false
-  end
-end
