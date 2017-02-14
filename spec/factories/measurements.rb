b07c02d9208800192539a48cb76b18d7d56c27dc
bosulliv42 <bosulliv@student.42.fr>
2017-02-14 09:47:04 +0100

    factory modif

--- spec/factories/measurements.rb
+++ spec/factories/measurements.rb
@@ -1,11 +1,9 @@
 FactoryGirl.define do
   factory :measurement do
     client_id 1
-    service_id 1
-    proxy_id 1
-    route_id 1
-    number_call 1
-    first_call_at "2017-02-13 10:44:11"
-    last_call_at "2017-02-13 10:44:11"
+    association           :service
+    association           :proxy
+    association           :route
+    requests_count 1
   end
 end
