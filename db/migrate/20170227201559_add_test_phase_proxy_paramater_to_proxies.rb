8cb3e20a4860bb28fac8d756431ae22a0ad19f0e
Jean-Michel Gigault <jgigault@student.42.fr>
2017-02-28 00:16:40 +0100

    fix migration

--- db/migrate/20170227201559_add_test_phase_proxy_paramater_to_proxies.rb
+++ db/migrate/20170227201559_add_test_phase_proxy_paramater_to_proxies.rb
@@ -1,10 +1,10 @@
 class AddTestPhaseProxyParamaterToProxies < ActiveRecord::Migration[5.0]
   def change
     add_column      :proxies, :proxy_parameter_test_id, :integer, null: true
+    remove_index :identifiers, name: 'index_identifiers_on_client_id_and_encrypted_secret'
 
     reversible do |direction|
       direction.up {
-        remove_index :identifiers, name: 'index_identifiers_on_client_id_and_encrypted_secret' if index_exists?(:identifiers, name: 'index_identifiers_on_client_id_and_encrypted_secret')
         Proxy.all.each do |proxy|
           proxy.proxy_parameter_test = proxy.proxy_parameter.dup
           proxy.proxy_parameter_test.identifier = proxy.proxy_parameter.identifier.dup unless proxy.proxy_parameter.identifier.nil?
