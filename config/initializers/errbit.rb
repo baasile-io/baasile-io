48925f5bacb2cc280f557f1eeb3b0829a9525ae7
Jean-Michel Gigault <jgigault@student.42.fr>
2017-03-01 10:50:19 +0100

    fix errbit configuration

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -2,7 +2,7 @@ Airbrake.configure do |config|
   config.environment = Rails.env
   config.ignore_environments = %w(development test)
   config.project_id = ENV['ERRBIT_PROJECT_ID'].to_i
-  config.api_key = ENV['ERRBIT_API_KEY']
+  config.project_key = ENV['ERRBIT_PROJECT_KEY']
   config.host    = ENV['ERRBIT_HOST']
   config.port    = ENV['ERRBIT_PORT'].to_i
   config.secure  = config.port == 443
