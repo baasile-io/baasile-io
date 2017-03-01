a0885742f34b494f5998c4cd2923aa01d088908b
Jean-Michel Gigault <jgigault@student.42.fr>
2017-03-01 10:52:31 +0100

    fix errbit configuration

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -4,6 +4,4 @@ Airbrake.configure do |config|
   config.project_id = ENV['ERRBIT_PROJECT_ID'].to_i
   config.project_key = ENV['ERRBIT_PROJECT_KEY']
   config.host    = ENV['ERRBIT_HOST']
-  config.port    = ENV['ERRBIT_PORT'].to_i
-  config.secure  = config.port == 443
 end
