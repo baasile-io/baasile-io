968c00ab4fff893069f242f8c64fc0752ce83d2e
bosulliv42 <bosulliv@student.42.fr>
2017-03-04 11:48:00 +0100

    repare view

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -99,19 +99,19 @@ class ContractsController < ApplicationController
 
   def commercial_reject
     if Contract.statuses[@contract.status] %2 == 1 && Contract.statuses[@contract.status] < 4
-      if @contract.is_commercial_client?(current_user)
+      if @contract.is_commercial?(current_user, :client)
         @contract.status = Contract.statuses.keys[Contract.statuses[@contract.status] - 2]
       end
     elsif Contract.statuses[@contract.status] < 4
-      if @contract.is_commercial_startup?(current_user)
+      if @contract.is_commercial?(current_user, :startup)
         @contract.status = Contract.statuses.keys[Contract.statuses[@contract.status] - 2]
       end
     elsif Contract.statuses[@contract.status] %2 == 1 && Contract.statuses[@contract.status] >= 4
-      if @contract.is_accounting_client?(current_user)
+      if @contract.is_accounting?(current_user, :client)
         @contract.status = Contract.statuses.keys[Contract.statuses[@contract.status] - 2]
       end
     elsif Contract.statuses[@contract.status] >= 4
-      if @contract.is_accounting_startup?(current_user)
+      if @contract.is_accounting?(current_user, :startup)
         @contract.status = Contract.statuses.keys[Contract.statuses[@contract.status] - 2]
       end
     end
