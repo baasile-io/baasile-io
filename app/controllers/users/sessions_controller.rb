b89a34007621d8a7aec9f784c3556b24df61f2ea
Jean-Michel Gigault <jgigault@student.42.fr>
2017-03-10 16:44:40 +0100

    skip set_paper_trail_whodunnit in sessions controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,4 +1,6 @@
 class Users::SessionsController < Devise::SessionsController
+  skip_before_filter :set_paper_trail_whodunnit
+
 # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
