48fcab5bfea7a5a700f7aad50183fa6ac227192e
bosulliv42 <bosulliv@student.42.fr>
2017-03-29 11:00:58 +0200

    litle fix

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
 
   belongs_to :user
-  has_many :proxies
+  has_many :proxies, dependent: :nullify
 
   validates :name, uniqueness: true, presence: true, length: {minimum: 2, maximum: 255}
   validates :description, presence: true, length: {minimum: 2, maximum: 255}
