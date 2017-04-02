52b8961aa625219f326b222f21a7a5119d773f09
bosulliv42 <bosulliv@student.42.fr>
2017-04-02 13:54:48 +0200

    accounting to accountant ok for mail main_commercial-accountant of service refered

--- app/mailers/contract_notifier.rb
+++ app/mailers/contract_notifier.rb
@@ -4,10 +4,16 @@ class ContractNotifier < ApplicationMailer
     @contract = contract
     @staging_name = staging_name
     @service = contract.send(staging[:can_edit])
-    users = @service.user_by_scope(staging[:scope])
-    users.each do |u|
-      mail( :to => u.email,
-            :subject => I18n.t("mailer.contract_notifier.send_by_status.#{@staging_name}.subject") )
+    scope_val = "main_#{staging[:scope]}"
+    user_by_scope = @service.send(scope_val.to_sym)
+    unless user_by_scope.nil?
+      mail( :to => user_by_scope.email,
+          :subject => I18n.t("mailer.contract_notifier.send_by_status.#{@staging_name}.subject") )
+    end
+    unless @service.user.nil?
+      mail( :to => @service.user.email,
+          :subject => I18n.t("mailer.contract_notifier.send_by_status.#{@staging_name}.subject") )
     end
   end
+
 end
