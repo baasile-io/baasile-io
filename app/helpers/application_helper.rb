b392b82847fe45976d5ca95479ac6e64b9f8eee1
Jean-Michel Gigault <jgigault@student.42.fr>
2017-04-08 11:01:39 +0200

    translations + remove old code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-  def arrange_nested_resources_collection(items, condition = nil)
+  def arrange_nested_resources_collection(items, proc_condition = nil)
     result = []
     items.map do |item|
-      if condition.nil? || condition.call(item)
+      if proc_condition.nil? || proc_condition.call(item)
         result << item
         #this is a recursive call:
-        result += arrange_nested_resources_collection(item.children, condition) {|i| i }
+        result += arrange_nested_resources_collection(item.children, proc_condition) {|i| i }
       end
     end
     result
--- app/helpers/inputs_helper.rb
+++ app/helpers/inputs_helper.rb
@@ -2,14 +2,4 @@ module InputsHelper
   def format_boolean_values_for_input
     [true, false].map {|v| [t("misc.#{v}"), v]}
   end
-
-  def format_nested_resources_for_select(items, current_id = nil, previous_disabled = false)
-    result = []
-    items.map do |item|
-      disabled = (previous_disabled ? previous_disabled : (item.id == current_id))
-      result << [item.name, item.id, {'data-depth': item.depth, 'data-icon': I18n.t("types.documentation_types.#{item.documentation_type}.icon"),disabled: disabled}]
-      result += format_nested_resources_for_select(item.children, current_id, disabled) if item.has_children?
-    end
-    result
-  end
 end
