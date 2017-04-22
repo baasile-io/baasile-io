1b83de009b0341785779f0af91becad6e9c88afd
Jean-Michel Gigault <jgigault@student.42.fr>
2017-04-22 17:50:27 +0200

    fix scope condition

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -38,7 +38,8 @@ module Api
                    }]
         }
       end
-      if !(authenticated_scope.include?(current_service.subdomain) || current_service.id == authenticated_service.id) && !(current_service.parent && (authenticated_scope.include?(current_service.parent.subdomain) || current_service.parent.id == authenticated_service.id))        return render status: 400, json: {
+      if !(authenticated_scope.include?(current_service.subdomain) || current_service.id == authenticated_service.id) && !(current_service.parent && (authenticated_scope.include?(current_service.parent.subdomain) || current_service.parent.id == authenticated_service.id))
+        return render status: 400, json: {
           errors: [{
                      status: 400,
                      title: "Unknown/invalid scope(s): #{authenticated_scope.inspect}. Required scope: \"#{current_service.subdomain}\"."
