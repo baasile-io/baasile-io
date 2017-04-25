e208e066bf23c457183597f9cbe0c5b3b269e084
bosulliv42 <bosulliv@student.42.fr>
2017-04-25 16:54:51 +0200

    mail send need locale

--- app/mailers/ticket_notifier.rb
+++ app/mailers/ticket_notifier.rb
@@ -1,29 +1,23 @@
 class TicketNotifier < ApplicationMailer
 
-  def send_ticket_notification(ticket, status, action)
+  def send_ticket_notification(ticket, action, list_users)
     @ticket = ticket
-    users_to_be_notified(ticket, status).each do |user|
+    users_to_be_notified(ticket, list_users).each do |user|
       locale = user.try(:language) || I18n.default_locale
       I18n.with_locale(locale) do
         @action_name = I18n.t("ticket.actions.#{action}.title")
         @status_name = I18n.t("types.ticket_statuses.#{ticket.ticket_status}.title")
         mail( to: user.email,
-              subject: I18n.t("mailer.ticket_notifier.send_ticket_notification.subject", status: @status_name, action: action) )
+              subject: I18n.t("mailer.ticket_notifier.send_ticket_notification.subject", status: @status_name, action: @action_name) )
       end
     end
   end
 
-  private
-
-  def users_to_be_notified(ticket, status)
-    users = []
-    Ticket::TICKET_STATUSES[status][:notifications].each do |user|
-      if user == 'superadmin'
-        users += User.with_role(:speradmin)
-      else
-        users << ticket.try(user.to_sym)
-      end
+  def users_to_be_notified(ticket, list_users)
+    list_res_of_users = []
+    list_users.each do |user_scope|
+      list_res_of_users << ticket.send(user_scope.to_s)
     end.reject(&:blank?).uniq
+    return list_res_of_users
   end
-
 end
