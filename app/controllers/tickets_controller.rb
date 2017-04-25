07d98435da447b645c03e3e06f9e739226039a40
bosulliv42 <bosulliv@student.42.fr>
2017-04-25 15:48:13 +0200

    transaction service ok need notification mail

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -5,8 +5,9 @@ class TicketsController < ApplicationController
   before_action :load_tickets, only: [:index]
   before_action :load_closed_tickets, only: [:list_closed]
   before_action :load_comments, only: [:show, :edit, :update, :destroy, :add_comment]
-  before_action :ticket_service, except: [:index, :list_closed]
+
   def add_comment
+    @ticket_service = Tickets::TicketService.new(@ticket)
     if @ticket_service.comment(params[:new_comment])
       flash[:success] = I18n.t('actions.success.updated', resource: t('activerecord.models.ticket'))
       redirect_to_show
@@ -39,10 +40,12 @@ class TicketsController < ApplicationController
     @ticket.user = current_user
     @ticket.ticket_status = Ticket::TICKET_STATUSES_ENUM[:opened]
     @ticket.assign_attributes(ticket_params)
+    @ticket_service = Tickets::TicketService.new(@ticket)
     if @ticket_service.create(params[:new_comment])
       flash[:success] = I18n.t('actions.success.created', resource: t('activerecord.models.ticket'))
       redirect_to_show
     else
+      flash[:fails] = I18n.t('errors.an_error_occured', resource: t('activerecord.models.ticket'))
       @new_comment = Comment.new(body: params[:new_comment])
       render :new
     end
@@ -55,6 +58,7 @@ class TicketsController < ApplicationController
   def update
     @ticket.assign_attributes(ticket_params)
     @ticket.ticket_status = Ticket::TICKET_STATUSES_ENUM[:opened]
+    @ticket_service = Tickets::TicketService.new(@ticket)
     if @ticket_service.edit(params[:new_comment])
       flash[:success] = I18n.t('actions.success.updated', resource: t('activerecord.models.ticket'))
       redirect_to_show
@@ -90,10 +94,6 @@ private
 
   def load_ticket
     @ticket = Ticket.find(params[:id])
-  end
-
-  def ticket_service
-    @ticket_service = Tickets::TicketService.new(@ticket)
   end
 
   def load_tickets
