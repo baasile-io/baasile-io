760751bb739919243b32940bd71c0845d2bf6a58
Jean-Michel Gigault <jgigault@student.42.fr>
2017-05-02 02:12:11 +0200

    bad fix of migration ?

--- db/migrate/20170501220738_update_old_objects_for_new_version.rb
+++ db/migrate/20170501220738_update_old_objects_for_new_version.rb
@@ -13,6 +13,11 @@ class UpdateOldObjectsForNewVersion < ActiveRecord::Migration[5.0]
           c.status = :waiting_for_production
           c.save
           puts c.price.inspect
+          if c.price.pricing_type.to_sym == :subscription && c.price.pricing_duration_type.to_sym == :prepaid
+            puts c.price.id
+            c.price.pricing_duration_type = :monthly
+            c.price.save!
+          end
           c.status = :validation_production
           c.save!
         end
--- db/schema.rb
+++ db/schema.rb
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 20170501220738) do
     t.datetime "updated_at",                      null: false
     t.string   "chamber_of_commerce", limit: 255
     t.index ["contactable_type", "contactable_id"], name: "index_contact_details_on_contactable_type_and_contactable_id", using: :btree
+    t.index ["name", "contactable_type", "contactable_id"], name: "id_contdetails_name_type_and_id", using: :btree
   end
 
   create_table "contracts", force: :cascade do |t|
@@ -170,7 +171,6 @@ ActiveRecord::Schema.define(version: 20170501220738) do
   create_table "identifiers", force: :cascade do |t|
     t.string   "client_id"
     t.string   "encrypted_secret"
-    t.datetime "expires_at"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.string   "identifiable_type"
@@ -354,8 +354,8 @@ ActiveRecord::Schema.define(version: 20170501220738) do
     t.datetime "created_at",                                     null: false
     t.datetime "updated_at",                                     null: false
     t.string   "subdomain"
-    t.integer  "company_id"
     t.boolean  "public",                         default: false
+    t.integer  "company_id"
     t.integer  "service_type",                   default: 1
     t.string   "ancestry"
     t.integer  "main_commercial_id"
@@ -366,6 +366,7 @@ ActiveRecord::Schema.define(version: 20170501220738) do
     t.index ["main_accountant_id"], name: "index_services_on_main_accountant_id", using: :btree
     t.index ["main_commercial_id"], name: "index_services_on_main_commercial_id", using: :btree
     t.index ["main_developer_id"], name: "index_services_on_main_developer_id", using: :btree
+    t.index ["name"], name: "index_services_on_name", unique: true, using: :btree
   end
 
   create_table "services_roles", id: false, force: :cascade do |t|
@@ -417,14 +418,14 @@ ActiveRecord::Schema.define(version: 20170501220738) do
     t.datetime "locked_at"
     t.datetime "created_at",                                       null: false
     t.datetime "updated_at",                                       null: false
-    t.datetime "password_changed_at"
-    t.string   "unique_session_id",      limit: 20
-    t.datetime "last_activity_at"
-    t.datetime "expired_at"
     t.string   "first_name"
     t.string   "last_name"
     t.integer  "gender"
     t.string   "phone"
+    t.datetime "password_changed_at"
+    t.string   "unique_session_id",      limit: 20
+    t.datetime "last_activity_at"
+    t.datetime "expired_at"
     t.boolean  "is_active",                         default: true
     t.string   "ancestry"
     t.string   "language",                          default: "en"
