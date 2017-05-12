edbadbbcbfbc50b6c0545d8605c8493fe98eb26f
Jean-Michel Gigault <jgigault@student.42.fr>
2017-05-12 10:11:12 +0200

    add debug and restrict rescue

--- app/controllers/concerns/error_measurement_concern.rb
+++ app/controllers/concerns/error_measurement_concern.rb
@@ -4,22 +4,25 @@ module ErrorMeasurementConcern
   private
 
   def do_request_error_measure(error, request_detail)
-    measure = ErrorMeasurement.create!(
-      contract: current_contract,
-      route: current_route,
-      error_type: error.class.name,
-      error_code: error.code,
-      response_http_status: (!error.res.nil? ? error.res.code : 0),
-      request_detail: request_detail.to_json
-    )
-    ErrorMeasurementNotifier.send_error_measurement_notification(measure, error.notifications).deliver_now
-    true
-  rescue Exception => e
-    Airbrake.notify('Failed to measure error', {
-      error: e.class,
-      message: e.message
-    })
-    false
+    begin
+      measure = ErrorMeasurement.create!(
+        contract: current_contract,
+        route: current_routes,
+        error_type: error.class.name,
+        error_code: error.code,
+        response_http_status: (!error.res.nil? ? error.res.code : 0),
+        request_detail: request_detail.to_json
+      )
+      ErrorMeasurementNotifier.send_error_measurement_notification(measure, error.notifications).deliver_now
+      true
+    rescue Exception => e
+      Rails.logger.error "Failed to measure error #{e.class} #{e.message}"
+      Airbrake.notify('Failed to measure error', {
+        error: e.class,
+        message: e.message
+      })
+      false
+    end
   end
 
 end
