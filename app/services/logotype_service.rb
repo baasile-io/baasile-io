bd2d73bebfa45aa82d08cf02bd378b2bb82ec53b
Jean-Michel Gigault <jgigault@student.42.fr>
2017-05-14 10:34:00 +0200

    refactor headers + nav

--- app/services/logotype_service.rb
+++ app/services/logotype_service.rb
@@ -94,7 +94,7 @@ class LogotypeService
     size_name = DEFAULT_SIZE unless SIZES.key?size_name
 
     aws_s3_object(client_id, size_name).exists?
-  rescue AWS::S3::Errors::Forbidden
+  rescue Aws::S3::Errors::Forbidden
     false
   end
 
