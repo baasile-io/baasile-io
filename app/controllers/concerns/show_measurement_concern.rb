e2d5660c8f5495554145704da09c93f5cd61b2ae
Jean-Michel Gigault <jgigault@student.42.fr>
2017-06-06 07:25:12 +0200

    aggregate measurement for companies

--- app/controllers/concerns/show_measurement_concern.rb
+++ app/controllers/concerns/show_measurement_concern.rb
@@ -6,8 +6,14 @@ module ShowMeasurementConcern
   end
 
   def init_measurement
-    @measures_output = Measurement.includes(:contract, :service, :proxy, :route, :measure_token).by_client(current_service).order(created_at: :asc)
-    @measures_input = Measurement.includes(:contract, :client, :proxy, :route, :measure_token).by_startup(current_service).order(created_at: :asc)
+    service_ids = if current_service.service_type.to_sym == :company
+                    current_service.subtree_ids
+                  else
+                    current_service.id
+                  end
+
+    @measures_output = Measurement.includes(:contract, :service, :proxy, :route, :measure_token).by_client(service_ids).order(created_at: :asc)
+    @measures_input = Measurement.includes(:contract, :client, :proxy, :route, :measure_token).by_startup(service_ids).order(created_at: :asc)
   end
 
 end
