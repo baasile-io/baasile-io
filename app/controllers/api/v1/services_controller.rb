9735bab843635336ac8dc34697f9f1d21a803481
bosulliv42 <bosulliv@student.42.fr>
2017-08-08 09:10:08 +0200

    function to get service in json

--- app/controllers/api/v1/services_controller.rb
+++ app/controllers/api/v1/services_controller.rb
@@ -6,54 +6,40 @@ module Api
       def index
         services = Service.activated_startups.published
         services_restrict = services.map do |service|
-          {
-            id: service.subdomain,
-            type: service.class.name,
-            attributes: {
-              name: service.name,
-              description: service.description,
-              website: service.website,
-              logotype: service.logotype_url(current_host),
-              proxies: service.proxies.map {|proxy|
-                {
-                  id: proxy.subdomain,
-                  type: proxy.class.name,
-                  attributes: {
-                    name: proxy.name,
-                    description: proxy.description,
-                    category: proxy.category.try(:name)
-                  }
-                }
-              }
-            }
-          }
+          get_service_into_json(service)
         end
         render json: {data: services_restrict}
       end
 
       def show
-        render json: {
-          data: {
-            id: current_service.subdomain,
-            type: current_service.class.name,
-            attributes: {
-              name: current_service.name,
-              description: current_service.description,
-              website: current_service.website,
-              logotype: current_service.logotype_url(current_host),
-              proxies: current_service.proxies.map {|proxy|
-                {
-                  id: proxy.subdomain,
-                  type: proxy.class.name,
-                  attributes: {
-                    name: proxy.name,
-                    description: proxy.description,
-                    category: proxy.category.try(:name)
-                  }
+        render json: get_service_into_json(current_service)
+      end
+
+      private
+
+      def get_service_into_json(service)
+        {
+            data: {
+                id: service.subdomain,
+                type: service.class.name,
+                attributes: {
+                    name: service.name,
+                    description: service.description,
+                    website: service.website,
+                    logotype: service.logotype_url(current_host),
+                    proxies: service.proxies.map {|proxy|
+                      {
+                          id: proxy.subdomain,
+                          type: proxy.class.name,
+                          attributes: {
+                              name: proxy.name,
+                              description: proxy.description,
+                              category: proxy.category.try(:name)
+                          }
+                      }
+                    }
                 }
-              }
             }
-          }
         }
       end
     end
